e6a63a802258c194445886c833dbfe5f618135cc
TheLordCoder <101518868+TheLordCoder@users.noreply.github.com>
2025-03-18 01:19:05 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -3,22 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
         <footer>
             <p>&copy; 2025 Verkkosivuni | Kaikki oikeudet pidätetään</p>
             <p>Seuraa minua:</p>
-            <a href="https://www.linkedin.com/in/KAYTTAJANIMI" target="_blank">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" 
-                    alt="LinkedIn" width="30" height="30">
-            </a>
-            <a href="https://github.com/KAYTTAJANIMI" target="_blank">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" 
-                    alt="GitHub" width="30" height="30">
-            </a>
+            <div class="social-links">
+                <a href="https://www.linkedin.com/in/KAYTTAJANIMI" target="_blank">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg" 
+                        alt="LinkedIn" class="social-icon">
+                </a>
+                <a href="https://github.com/KAYTTAJANIMI" target="_blank">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" 
+                        alt="GitHub" class="social-icon">
+                </a>
+            </div>
         </footer>
     `;
 
-<li>
-<a href="https://www.linkedin.com/in/felix-ayravainen" target="_blank">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg" class="linkedin-logo" alt="LinkedIn">
-</a>
-</li>
-    
     document.body.insertAdjacentHTML("beforeend", footerHTML);
 });
