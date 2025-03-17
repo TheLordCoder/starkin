6f3d080c65d4075e704725f649d361bc685d71b3
TheLordCoder <101518868+TheLordCoder@users.noreply.github.com>
2025-03-18 01:27:58 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const footerHTML = `
         <footer>
-            <p>&copy; 2025 Verkkosivuni | Kaikki oikeudet pidätetään</p>
-            <p>Seuraa minua:</p>
+            <p>&copy; 2025 | All rights reserved</p>
+            <p>More information:</p>
             <div class="social-links">
                 <a href="https://www.linkedin.com/in/felix-ayravainen" target="_blank">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg" 
