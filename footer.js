5e11ec107fa570f59bfd39467126f009e3ae4890
TheLordCoder <101518868+TheLordCoder@users.noreply.github.com>
2025-03-18 01:06:10 +0200

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const footerHTML = `
+        <footer>
+            <p>&copy; 2025 Verkkosivuni | Kaikki oikeudet pidätetään</p>
+            <p>Seuraa minua:</p>
+            <a href="https://www.linkedin.com/in/KAYTTAJANIMI" target="_blank">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" 
+                    alt="LinkedIn" width="30" height="30">
+            </a>
+            <a href="https://github.com/KAYTTAJANIMI" target="_blank">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" 
+                    alt="GitHub" width="30" height="30">
+            </a>
+        </footer>
+    `;
+
+    document.body.insertAdjacentHTML("beforeend", footerHTML);
+});
