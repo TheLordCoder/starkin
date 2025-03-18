97d0a4255ef73b7463e9210f92aa8807afbbd2aa
TheLordCoder <101518868+TheLordCoder@users.noreply.github.com>
2025-03-18 11:21:03 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const footerHTML = `
-        <footer>
-            <p>&copy; 2025 | All rights reserved</p>
-            <p>More information:</p>
-            <div class="social-links">
-                <a href="https://www.linkedin.com/in/felix-ayravainen" target="_blank">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg" 
-                        alt="LinkedIn" class="social-icon">
-                </a>
-            </div>
-        </footer>
-    `;
+    // tarkistaaa, onko footer jo olemassa
+    if (!document.querySelector("footer")) {
+        const footerHTML = `
+            <footer>
+                <p>&copy; 2025 | All rights reserved</p>
+                <p>More information:</p>
+                <div class="social-links">
+                    <a href="https://www.linkedin.com/in/felix-ayravainen" target="_blank">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg" 
+                            alt="LinkedIn" class="social-icon">
+                    </a>
+                </div>
+            </footer>
+        `;
 
-    document.body.insertAdjacentHTML("beforeend", footerHTML);
+        document.body.insertAdjacentHTML("beforeend", footerHTML);
+    }
 });
